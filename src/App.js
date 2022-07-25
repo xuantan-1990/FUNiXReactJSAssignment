211d3eb32fd864900d5158fb2f53134671faafea
Ho Xuan Tan <tanhxFX15168@funix.edu.vn>
2022-07-25 21:03:47 +0700

    commit Navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
-        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+     return (
+       <div className="App">
+         <Navbar dark color="primary">
+           <div className="container">
+             <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+           </div>
+         </Navbar>
+       </div>
+     );
+  }
+ 
 }
 
 export default App;
