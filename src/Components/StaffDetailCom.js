6b32d8ae2c158b93961425eacae8b4d28931730b
Ho Xuan Tan <tanhxFX15168@funix.edu.vn>
2022-08-07 09:53:25 +0700

    can chinh giao dien StaffDetail

--- src/Components/StaffDetailCom.js
+++ src/Components/StaffDetailCom.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import {
-  Card,
-  CardImg,
-  CardBody,
   Breadcrumb,
-  BreadcrumbItem,
+  BreadcrumbItem, Row
 } from "reactstrap";
 import dateFormat from "dateformat";
 import { Link } from "react-router-dom";
 
 function RenderStaff({ staff }) {
   return (
-    <div className="col-12">
-      <Card>
-        <CardImg width="100%" src={staff.image} value={staff.name} />
-        <CardBody>
+    <div>
+      <Row>
+        <div className="col-12 col-md-5 col-xl-5">
+          <img width="100%" float="right" src={staff.image} alt={staff.name} />
+        </div>
+        <div className="col-12 col-md-7 col-xl-7">
           <h3>Họ và tên: {staff.name}</h3>
           <p>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</p>
           <p>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</p>
           <p>Phòng ban: {staff.department.name}</p>
           <p>Số ngày nghỉ còn lại: {staff.annualLeave}</p>
           <p>Số ngày đã làn thêm: {staff.overTime}</p>
-        </CardBody>
-      </Card>
+        </div>
+      </Row>
     </div>
   );
 }
@@ -38,13 +37,11 @@ export default function StaffDetail(props) {
                 </BreadcrumbItem>
                 <BreadcrumbItem active>{props.nhanvien.name}</BreadcrumbItem>
               </Breadcrumb>
-              <div className="col-12">
-                <h3>{props.nhanvien.name}</h3>
                 <hr />
-              </div>
+              
             </div>
             <div className="row">
-              <div className="col-12 col-md-5 m-1">
+              <div className="col-12 col-md-7 m-1">
                 <RenderStaff staff={props.nhanvien} />
               </div>
             </div>
