09790be1389f99273d1e857ddd0fb99c60d6b6bc
Ho Xuan Tan <tanhxFX15168@funix.edu.vn>
2022-08-07 10:06:53 +0700

    giao dien Departments

--- src/Components/DepartmentCom.js
+++ src/Components/DepartmentCom.js
@@ -8,16 +8,18 @@ export default class Departments extends Component {
   render() {
     const Depart = this.props.departments.map((department) => {
       return (
-        <div key={department.id} className="col-6 col-md-4 col-xl-2 mb-3">
+        <div key={department.id} className="col-12 col-md-6 col-xl-4 mb-3">
           <Card>
-            <CardTitle>{department.name}</CardTitle>
-            <CardTitle>{department.numberOfStaff}</CardTitle>
+            <CardTitle className="AppLeft ml-1">
+              <h2>{department.name}</h2>
+            </CardTitle>
+            <CardTitle className="AppLeft ml-5">Số lượng nhân viên: {department.numberOfStaff}</CardTitle>
           </Card>
         </div>
       );
     });
     return (
-      <div className="container col-9">
+      <div className="container col-12">
         <div className="row">{Depart}</div>
       </div>
     );
